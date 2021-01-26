79d33157b977b02ac7daee02c0e2d55afe05fe7a
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-01-26 11:00:34 +0800

    Create promoModel.py

---
+++ promoModules/promoModel.py
@@ -0,0 +1,18 @@
+# getting any year, any sem promoModules
+
+import re
+
+def promo(year, sem=0):
+    if s == "both": s = 12
+    with open("nushModules.csv") as modules, open("y"+str(year)+("s"+sem if sem else "")+"Modules.csv", "w+") as out:
+        print(modules.readline(), file=out)
+
+        for i in modules.readlines()[1:]:
+            code, mc, name, y, s = i.strip().split(",")
+            if y == str(year):
+                if sem == 0:
+                    print(i, file=out)
+                elif sem == 2:
+                    if str(sem) in s: print(i, file=out)
+                elif s == str(sem):
+                    print(i, file=out)
