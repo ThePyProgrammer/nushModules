45743cd3d97e2078af20a48d596e5a1d368e427e
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-01-26 11:06:57 +0800

    Update promoModel.py

--- promoModules/promoModel.py
+++ promoModules/promoModel.py
@@ -1,14 +1,15 @@
 # getting any year, any sem promoModules
 
-def promo(year, sem=0):
-    if s == "both": s = 12
-    with open("nushModules.csv") as modules, open("y"+str(year)+("s"+sem if sem else "")+"Modules.csv", "w+") as out:
-        print(modules.readline(), file=out)
+def promo(year, sem=-1):
+    if sem == "both": sem = 0
+    with open("nushModules.csv") as modules, open("y"+str(year)+("s"+str(sem) if sem+1 else "")+"Modules.csv", "w+") as out:
+        print(modules.readline().strip(), file=out)
 
         for i in modules.readlines()[1:]:
-            code, mc, name, y, s = i.strip().split(",")
+            i = i.strip()
+            code, mc, name, y, s = i.split(",")
             if y == str(year):
-                if sem == 0:
+                if sem == -1:
                     print(i, file=out)
                 elif sem == 2:
                     if str(sem) in s: print(i, file=out)
@@ -16,5 +17,5 @@ def promo(year, sem=0):
                     print(i, file=out)
 
 for year in range(1, 7):
-    for sem in [0, 1, 2, "both"]:
+    for sem in range(-1, 3):
         promo(year, sem)
